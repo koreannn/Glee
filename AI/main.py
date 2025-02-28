4afa7a55891eb4d8a0eefd17c8cbca002de681d4
koreannn <ghdtjdwo5@gamil.com>
2025-02-28 19:41:54 +0900

    video search 관련 테스트 이상 없음

--- AI/main.py
+++ AI/main.py
@@ -4,17 +4,15 @@ def get_user_choice():
     print("2. 사진 첨부하지 않음 (직접 텍스트 입력)")
     choice = input("선택 (1 또는 2): ")
     return choice.strip()
-
-
 from loguru import logger
 
-from videosearch_service import VideoSearchService
+from services.videosearch_service import VideoSearchService
 
-from ocr_v2 import (
-    analyze_situation_accent_purpose,
-    generate_reply_suggestions_detail,
-    analyze_situation,
-)
+# from ocr_v2 import (
+#     analyze_situation_accent_purpose,
+#     generate_reply_suggestions_detail,
+#     analyze_situation,
+# )
 
 
 # test1: 이미지 -> 상황파악
@@ -51,20 +49,20 @@ def main():
 
     video_search_service = VideoSearchService()
 
-    # # test1: URL로 비디오 ID 추출 테스트
-    # # test2: 해당 영상이 자막 정보를 제공하는지 여부 확인 동작 테스트
+    # test1: URL로 비디오 ID 추출 테스트
+    # test2: 해당 영상이 자막 정보를 제공하는지 여부 확인 동작 테스트
     # test_url = "https://www.youtube.com/watch?v=P9qmSnsyFS0"
     # video_id = video_search_service.extract_video_id(test_url)
-    # captions_available = video_search_service.check_english_captions_available(video_id)
+    # captions_available = video_search_service.check_captions_available(video_id)
     # logger.info(f"추출된 비디오 ID: {video_id}")
     # logger.info(f"자막 유무 정보: {captions_available}")
 
-    # test3: 검색 쿼리로 영상 검색 테스트
-    # test4: 영상에 대한 자막 텍스트 추출 테스트
-    test_query = "사과 영양성분"
-    video_info = video_search_service.youtube_search(test_query)
-    video_caption = video_search_service.get_video_transcripts(video_info)
-    logger.info(f"자막 텍스트: {video_caption}")
+    # # test3: 검색 쿼리로 영상 검색 테스트
+    # # test4: 영상에 대한 자막 텍스트 추출 테스트
+    # test_query = "사과 영양성분"
+    # video_info = video_search_service.youtube_search(test_query)
+    # video_caption = video_search_service.get_video_transcripts(video_info)
+    # logger.info(f"자막 텍스트: {video_caption}")
 
 
 if __name__ == "__main__":
