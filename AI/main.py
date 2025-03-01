cddd2c18cfae6022bb04b95539cb24ef791cbde8
koreannn <ghdtjdwo5@gamil.com>
2025-03-01 23:55:55 +0900

    chore:Fix Style

--- AI/main.py
+++ AI/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+
 def get_user_choice():
     print("사진 첨부 OR 사진 미 첨부")
     print("1. 사진 첨부 (OCR 처리)")
@@ -6,9 +10,11 @@ def get_user_choice():
     return choice.strip()
 
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from loguru import logger
 
-from AI.services.videosearch_service import VideoSearchService
+# from AI.services.videosearch_service import VideoSearchService
 
 from ocr_v2 import (
     CLOVA_AI_Situation_Summary,
@@ -20,27 +26,47 @@ from ocr_v2 import (
     analyze_situation_accent_purpose,
     generate_suggestions_situation,
     generate_reply_suggestions_accent_purpose,
-    New_Reply_Suggestions_Detailed,
+    generate_reply_suggestions_detail,
+    CLOVA_OCR,
 )
-from services.title_suggestion import CLOVA_AI_Title_Suggestions
+from AI.services.title_suggestion import CLOVA_AI_Title_Suggestions
 
 
 def main():
-    # # 1. 개별 메서드 출력 테스트
+    """
+    개별 메서드 출력 테스트
+    """
     # CLOVA_AI_Situation_Summary("아, 배고프다.")
     # CLOVA_AI_Title_Suggestions("아, 배고프다.")
     # CLOVA_AI_Reply_Suggestions("식사 시간이 다가오면 배고픔을 느끼는 것은 자연스러운 일이죠. 식사를 할 수 없는 상황이라면 간단한 간식이나 음료를 섭취하여 급한 배고픔을 해결할 수도 있습니다. 만약 지속적인 배고픔 때문에 고민이시라면 건강 상태나 생활 습관을 한 번 되돌아보시는 건 어떠신가요?")
     # CLOVA_AI_New_Reply_Suggestions("배고픈 상황", "친절하게", "카카오톡", "친절하지만 퉁명스럽게 말해주세요" )
     # CLOVA_AI_Style_Analysis("아, 배고프다.")
-    CLOVA_AI_Title_Suggestions(
-        "식사 시간이 되었다면, 건강하고 맛있는 식사를 챙겨 드시는 것은 어떠신가요?식사 시간이 되었다면, 건강하고 맛있는 식사를 챙겨 드시는 것은 어떠신가요?"
-    )
+    # CLOVA_AI_Title_Suggestions(
+    #     "식사 시간이 되었다면, 건강하고 맛있는 식사를 챙겨 드시는 것은 어떠신가요?식사 시간이 되었다면, 건강하고 맛있는 식사를 챙겨 드시는 것은 어떠신가요?"
+    # )
 
     # generate_suggestions_situation("아, 자고싶다.")
     # generate_reply_suggestions_accent_purpose("아, 자고싶다.", "친절하게", "카카오톡")
-    # New_Reply_Suggestions_Detailed("아, 자고싶다.", "친절하게", "카카오톡", "자고싶다는 말을 친절하고 차분하게 전달하고싶어요")
+    # generate_reply_suggestions_detail("아, 자고싶다.", "친절하게", "카카오톡", "자고싶다는 말을 친절하고 차분하게 전달하고싶어요")
 
-    # # test1: 이미지 -> 상황파악
+    """
+    OCR 테스트
+    """
+    image_files = []
+    test_images = ["./AI/OCR_Test1.png", "./AI/OCR_Test2.png", "./AI/OCR_Test3.png", "./AI/OCR_Test4.png"]
+
+    for image_path in test_images:
+        try:
+            with open(image_path, "rb") as f:
+                file_content = f.read()
+                image_files.append((image_path, file_content))
+        except Exception as e:
+            logger.error(f"파일 읽기 오류({image_path}): {e}")
+            continue
+
+    CLOVA_OCR(image_files)
+
+    # # test2: 이미지 -> 상황파악 + 말투 + 용도
     # image_files = []
     # test_images = ["./AI/OCR_Test1.png", "./AI/OCR_Test2.png", "./AI/OCR_Test3.png", "./AI/OCR_Test4.png"]
 
@@ -53,10 +79,7 @@ def main():
     #         logger.error(f"파일 읽기 오류({image_path}): {e}")
     #         continue
 
-    # analyze_situation(image_files)
-
-    # # test2: 이미지 -> 상황파악 + 말투 + 용도
-    # Situation_Accent_Purpose(["./AI/OCR_Test1.png","./AI/OCR_Test2.png","./AI/OCR_Test3.png","./AI/OCR_Test4.png"])
+    # analyze_situation_accent_purpose(image_files)
 
     # # test3: 이미지 -> 답변 추천
     # print(Reply_Suggestions(["./OCR_Test1.png","./OCR_Test2.png","./OCR_Test3.png","./OCR_Test4.png"]))
@@ -73,6 +96,9 @@ def main():
     # detail = input()
     # print(New_Reply_Suggestions_Detailed(situation, accent, purpose, detail))
 
+    """
+    youtube 영상 검색 테스트
+    """
     # # 로거 설정
     # logger.add("logs/app.log", rotation="500 MB")
 
